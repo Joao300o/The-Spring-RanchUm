cbbc8802274b12ae4b79d156e40283203005dd74
João Vitor Gonçalves <joaogaeww@gmail.com>
2026-08-18 17:12:00 -0300

    0.3.1
    Sistema de plantacao 60%.

--- The Spring Ranch/Assets/Scripts/FarmGrid.cs
+++ The Spring Ranch/Assets/Scripts/FarmGrid.cs
@@ -1,24 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;  
 
 public class FarmGrid : MonoBehaviour
 {
   public float cellSize = 1f;
   public float plantingDistancie = 3f;
 
-  public Transform playerTransform;
+  public Transform playerTransform, gridSelector;
 
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
 
         if(Physics.Raycast(ray, out RaycastHit hit))
         {
             Vector3 mousePosition = hit.point;
 
-            int gridX = Mathf.FloorToInt(mousePosition.x / cellSize);)
+            int gridX = Mathf.FloorToInt(mousePosition.x / cellSize);
             int gridZ = Mathf.FloorToInt(mousePosition.z / cellSize);
 
-            Vector3 cellPosition = new Vector3(grid)
-        })
+            Vector3 cellPosition = new Vector3(gridX * cellSize, 0, gridZ * cellSize);
+
+            cellPosition += new Vector3(cellSize / 2, 0, cellSize / 2);
+            gridSelector.position = cellPosition;
+
+            float distancePlayer = Vector3.Distance(playerTransform.position, cellPosition);
+        }
     }
 }
